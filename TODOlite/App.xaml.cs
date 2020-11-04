024283097dfe190a9b71533565b3c873ab20d988
Marijo Blažević <mblaze2@racunarstvo.hr>
2020-11-04 18:57:23 +0100

    Rename

--- TODOlite/App.xaml.cs
+++ TODOlite/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace TODOlight
+namespace TODOlite
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- TODOlite/Properties/Settings.Designer.cs
+++ TODOlite/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TODOlight.Properties {
+namespace TODOlite.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
